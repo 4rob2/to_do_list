434eb953519e9ce7cac022590f939c35a4874873
Joseph Karasek and rob reasoner <reasoner55@hotmail.com>
2016-01-25 13:15:46 -0800

    add more html, to do list now populates with checkbox and secondary information

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     var itemName2 = $("#items2").val();
     var newTask = new Task(itemName, itemName2);
 
-    $('#toDoList').append("<li><span class='toDoItem'>" + newTask.item + "</span></li>");
+    $('#toDoList').append("<li class='toDoItem'><input type='checkbox'><span>" + newTask.item + "</span></li><br><span class='toDoDescription'>" + newTask.secondary + "</span>");
 
     $('.toDoItem').last().click(function() {
       // toggle the done or not
